65007b787e96f7719e3338b1ef09a162bdfd42e9
mtkuhn <junkpilemk@protonmail.com>
2024-12-01 07:57:01 -0700

    Day 1 Kotlin

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,9 +1,27 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     val input = readInput("Day01")
+    val testInput = readTestInput("Day01")
+    println(day1part1(testInput))
     println(day1part1(input))
+    println(day1part2(testInput))
     println(day1part2(input))
 }
 
-fun day1part1(input: List<String>): Int = 0
+fun day1part1(input: List<String>): Int {
+    val left = input.listLeft().sorted()
+    val right = input.listRight().sorted()
+    return left.zip(right).sumOf { (it.first - it.second).absoluteValue }
+}
 
-fun day1part2(input: List<String>): Int = 0
+fun day1part2(input: List<String>): Int {
+    val left = input.listLeft().sorted()
+    val right = input.listRight().sorted()
+    val rFreq = right.frequencyMap()
+    return left.sumOf { it * (rFreq[it] ?: 0) }
+}
+
+fun List<String>.listLeft() = map { it.split("\\s+".toRegex())[0].toInt() }
+fun List<String>.listRight() = map { it.split("\\s+".toRegex())[1].toInt() }
+fun List<Int>.frequencyMap() = fold(mutableMapOf<Int, Int>()) { acc, i -> acc.apply { acc[i] = (acc[i]?:0)+1 } }
